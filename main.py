6af8d617dcab653b346965e7362cb112f569d922
Ankitdubey040 <ankitdubey04052001@gmail.com>
2022-12-20 01:02:02 +0530

    2.24.11 tak_ka upto .env killed

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
 import sys,os
+from sensor.utils import get_collection_as_dataframe
+
 
 
 
 
 if __name__=="__main__":
      try:
-          pass
+          get_collection_as_dataframe(database_name = "aps",collection_name = "sensor")
+          
      except Exception as e:
           print(e)
---
+++ sensor/__init__.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+# to read from the .env file 
+
+print(f"Loadinng environment variables from .env file")
+
+load_dotenv()

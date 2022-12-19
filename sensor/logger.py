652264a6ca7e049f74bad62b42f310b83b08a868
Ankitdubey040 <ankitdubey04052001@gmail.com>
2022-12-19 23:55:24 +0530

    test_logger_and_exception_performed

---
+++ sensor/logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from datetime import datetime
+
+
+#every time create a new log file , log file name 
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%y_%H%M%S')}.log"
+
+# log file directory
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
+
+# create a dir of Log Files if not exist
+os.makedirs(LOG_FILE_DIR,exist_ok = True)
+ 
+#log file path 
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+#logging function : 
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format = " [%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s ",
+    level = logging.DEBUG,
+
+)
+
+
+
+
+
